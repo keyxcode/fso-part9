c4a7a08490c8891911102080c35853aed44eddf5
keyxcode <keyxcode@gmail.com>
2023-05-15 16:04:14 +0700

    Refactor  bmiCalculator to use command line args

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -21,4 +21,31 @@ const calculateBmi = (heightCm: number, weightKg: number): string => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+interface ParsedValues {
+  heightCm: number;
+  weightKg: number;
+}
+
+const parseArguments = (args: string[]): ParsedValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+  if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) {
+    throw new Error("Provided values were not numbers!");
+  }
+
+  return {
+    heightCm: Number(args[2]),
+    weightKg: Number(args[3]),
+  };
+};
+
+try {
+  const { heightCm, weightKg } = parseArguments(process.argv);
+  console.log(calculateBmi(heightCm, weightKg));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
